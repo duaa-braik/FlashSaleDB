d1d65ccf5c7f2ff94938d14ef020ef7c144edb34
duaa-braik <duaa13braik@gmail.com>
2025-10-26 20:58:47 +0200

    Add DbContext class and configurations

---
+++ FlashSaleDB.Infrstructure/FlashSaleDbContext.cs
@@ -0,0 +1,80 @@
+using FlashSaleDB.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlashSaleDB;
+
+public class FlashSaleDbContext: DbContext
+{
+    public DbSet<User> User { get; set; }
+
+    public DbSet<Cart> Cart { get; set; }
+
+    public DbSet<Product> Product { get; set; }
+
+    public DbSet<Inventory> Inventory { get; set; }
+
+    public DbSet<Sale> Sale { get; set; }
+
+    public DbSet<Reservation> Reservation { get; set; }
+
+    public DbSet<Payment> Payment { get; set; }
+
+    public FlashSaleDbContext(DbContextOptions<FlashSaleDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .HasMany(p => p.Carts)
+            .WithMany(c => c.Products)
+            .UsingEntity<CartItem>(
+                r => r.HasOne<Cart>().WithMany().HasForeignKey(e => e.CartId),
+                l => l.HasOne<Product>().WithMany().HasForeignKey(e => e.ProductId));
+        
+        modelBuilder.Entity<Inventory>()
+            .HasKey(i => new { i.Id, i.ProductId });
+
+        modelBuilder.Entity<Cart>()
+            .Property(c => c.Id)
+            .HasMaxLength(36);
+        
+        modelBuilder.Entity<Order>()
+            .Property(o => o.Id)
+            .HasMaxLength(36);
+        
+        modelBuilder.Entity<Payment>()
+            .Property(p => p.Id)
+            .HasMaxLength(36);
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Id)
+            .HasMaxLength(36);
+        
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Name)
+            .HasMaxLength(100);
+        
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Category)
+            .HasMaxLength(50);
+        
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Description)
+            .HasMaxLength(2048);
+        
+        modelBuilder.Entity<User>()
+            .Property(u => u.Id)
+            .HasMaxLength(36);
+        
+        modelBuilder.Entity<User>()
+            .Property(u => u.Name)
+            .HasMaxLength(100);
+        
+        modelBuilder.Entity<User>()
+            .Property(u => u.Email)
+            .HasMaxLength(100);
+        
+        modelBuilder.Entity<User>()
+            .Property(u => u.Password)
+            .HasMaxLength(500);
+    }
+}
