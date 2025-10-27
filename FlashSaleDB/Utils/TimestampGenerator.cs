f9529af186da85dea8baff96e4c21d9dbc0b15c7
duaa-braik <duaa13braik@gmail.com>
2025-10-27 20:20:03 +0200

    Add Timestamp and UUID generators

---
+++ FlashSaleDB/Utils/TimestampGenerator.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace FlashSaleDB.Utils;
+
+public class TimestampGenerator : ValueGenerator<DateTime>
+{
+    public override bool GeneratesTemporaryValues => false;
+
+    public override DateTime Next(EntityEntry entry)
+    {
+        return new DateTime();
+    }
+}
